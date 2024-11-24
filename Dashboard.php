d8f22ab64f0fe1bec9950720bc0a9e72d4eb1c71
davicorr3a <davicorrea43@gmail.com>
2024-11-24 20:47:42 -0300

    arquivos

---
+++ Dashboard.php
@@ -0,0 +1,9 @@
+<?php
+include_once("templates/header.php");
+?>
+
+<h1>Dashboard</h1>
+<!--Esse código HTML é usado para exibir um ícone de fonte proveniente da biblioteca Font Awesome.-->
+<?php
+include_once("templates/footer.php");
+?>
